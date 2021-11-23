80f0e76c44a4e52425645a8364e62130671eba77
Andres Robles <roblesandresm1@gmail.com>
2021-11-22 19:49:30 -0500

    first online server

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require_once "controllers/template_controller.php";
+
+$template = new ControllerTemplate();
+$template->ctrTemplate();
---
+++ views/template.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS CDN -->
+    <!--
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Inicio | Dashboard</title>
+  </head>
+  <body>
+    <!-- Header -->
+    <div class="container-fluid">
+        <div class="row justify-content-center align-content-center">
+            <div class="col-8 barra">
+                <h2><span>B</span>ind<span>S</span>olutions</h2>
+            </div>
+            <div class="col-4 text-right barra">
+                <ul class="navbar-nav mr-auto">
+                    <li>
+                        <a 
+                            href="#" 
+                            class="px-3 text-light perfil dropdown-toggle" 
+                            id="navbar-Dropdown" 
+                            role="button" data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                        >
+                            <i class="fas fa-user-circle user"></i>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbar-dropdown">
+                            <a href="#" class="dropdown-item menu-perfil cerrar">
+                                <i class="fas fa-sign-out-alt m-1"></i>Cerrar sesion
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <!-- MAIN -->
+    <div class="container-fluid">
+        <div class="row">
+            <!-- BARRA LATERAL-->
+            <div class="barra-lateral col-12 col-sm-auto">
+                <nav class="menu d-flex d-sm-block justify-content-center flex-wrap">
+                    <a href="#" alt="Inicio" class="active"><i class="fas fa-home"></i><span>Inicio</span></a>
+                    <a href="#" alt="Usuarios"><i class="fas fa-users"></i><span>Usuarios</span></a>
+                    <a href="#"><i class="fab fa-product-hunt"></i><span>Productos</span></a>
+                    <a href="#"><i class="fas fa-calendar-check"></i><span>Eventos</span></a>
+                    <a href="#"><i class="fas fa-store"></i><span>Puntos de venta</span></a>
+                    <a href="#"><i class="fas fa-dollar-sign"></i><span>Ventas</span></a>
+                </nav> 
+            </div>
+            <!-- CONTENIDO PRINCIPAL -->
+            <main class="main col">
+                <div class="row justify-content-center align-content-center text-center">
+                    <div class="columna col-lg-6">
+                        CONTENIDO
+                    </div>
+                </div>
+            </main>
+        </div>
+    </div>
+
+
+    <!-- Sources hosting -->
+    <script src="assets/js/jquery-3.6.0.slim.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/c5430e362e.js" crossorigin="anonymous"></script>
+    
+    <!-- CDN: jQuery and Bootstrap Bundle (includes Popper) -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
